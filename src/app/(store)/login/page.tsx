11853c77f8ba6e18f49db9cb245a8a18540bc957
Nikunj Koladiya <144877113+koladiya-nikunj@users.noreply.github.com>
2026-08-10 19:35:46 +0530

    db atlas link added

--- src/app/(store)/login/page.tsx
+++ src/app/(store)/login/page.tsx
@@ -147,7 +147,7 @@ export default function LoginPage() {
                                 completeLogin();
                             }
                         },
-                        { maximumAge: 10000, timeout: 3000, enableHighAccuracy: false }
+                        { maximumAge: 7000, timeout: 3000, enableHighAccuracy: false }
                     );
                 } else {
                     completeLogin();
